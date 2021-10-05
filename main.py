d2b0d94d13a35805132b2a7d79f8901e24824b11
CainBurt <cain.m.burt@gmail.com>
2021-10-05 13:31:42 +0000

    Deployed the website

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ app = flask.Flask(__name__)
 @app.route("/", methods=["GET"]) 
 def hello(): 
     """ Return a friendly HTTP greeting. """ 
-    return "Hello World!\n" 
+    who = flask.request.args.get("who", "World") 
+    return f"Hello {who}!\n" 
  
 if __name__ == "__main__": 
     # Used when running locally only. When deploying to Google App 
